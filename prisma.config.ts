ad8bde112af62bd4f4fe794b70a2e7772f3294cb
Leonam Brites <102072289+leonambrites@users.noreply.github.com>
2026-08-31 10:03:23 -0300

    fix: atualizado prisma.config.ts com especificacao do Prisma 7 e tratamento de variaveis via process.env

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    seed: 'tsx prisma/seed.ts',
+  },
   datasource: {
-    url: env('POSTGRES_PRISMA_URL') || env('DATABASE_URL') || env('bd_church_PRISMA_DATABASE_URL'),
+    url:
+      process.env.POSTGRES_PRISMA_URL ||
+      process.env.DATABASE_URL ||
+      process.env.bd_church_PRISMA_DATABASE_URL ||
+      process.env.bd_church_POSTGRES_URL ||
+      '',
   },
 });
